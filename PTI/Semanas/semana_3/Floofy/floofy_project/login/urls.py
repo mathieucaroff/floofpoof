3194388e294980ceabebc6ae1dcb833ae7a3b2d8
fc51648 <fc51648@alunos.fc.ul.pt>
2020-03-03 00:13:15 +0000

    sistema login funciona

--- PTI/Semanas/semana_3/Floofy/floofy_project/login/urls.py
+++ PTI/Semanas/semana_3/Floofy/floofy_project/login/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='login-home'),
+    #path('', views.home, name='login-home'),
     path('login-student/', views.logstudent, name='login-student'),
     path('login-teacher/', views.logteacher, name='login-teacher'),
+    path('logged-student/', views.loggedstudent, name='logged-student'),
+    path('logged-teacher/', views.loggedteacher, name='logged-teacher'),
+    path('login/', views.log_in, name='login'),
+    path('', views.log_in, name='login-home'),
 ]
--- PTI/Semanas/semana_3/Floofy/floofy_project/login/views.py
+++ PTI/Semanas/semana_3/Floofy/floofy_project/login/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User as default_user
+from django.contrib.auth import authenticate, login
+from login.models import User, Student, Teacher
 
 def home(request):
     return render(request, 'login/index.html')
@@ -9,3 +12,37 @@ def logstudent(request):
 
 def logteacher(request):
     return render(request, 'login/login_teacher.html')
+
+def loggedstudent(request):
+    return render(request, 'login/logged_student.html')
+
+def loggedteacher(request):
+    return render(request, 'login/logged_teacher.html')
+
+def log_in(request):
+    if (request.method == 'POST'):
+        
+        username = request.POST.get('username') #Get username value from form
+        password = request.POST.get('password') #Get password value from form
+        user = authenticate(request, username=username, password=password)
+
+        print(str(username))
+        print(str(password))
+
+        print(User.objects.get(username=username))
+
+
+        if user is not None:
+            print("not none")
+            login(request, user)
+            user_obj = User.objects.get(username=username)
+            if user.is_authenticated and user_obj.is_student:
+                return redirect('logged-student') #Go to student home
+            elif user.is_authenticated and user_obj.is_teacher:
+                return redirect('logged-teacher') #Go to teacher home
+        else:
+            # Invalid email or password. Handle as you wish
+
+            return redirect('login-home')
+            
+    return render(request, 'login/login_home.html')
