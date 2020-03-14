40a654e404f4b6420cfb2696a35aca3d448470d7
fc51648 <fc51648@alunos.fc.ul.pt>
2020-03-14 04:31:39 +0000

    melhorado caso de uso definir regras de grupos, apagado detailview

--- PTI/Semanas/semana_4/Floofy/floofy_project/groups/urls.py
+++ PTI/Semanas/semana_4/Floofy/floofy_project/groups/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 from .views import (
-    Select_Subject,
-    Set_Rules,
     Set_Stages
 )
 
 urlpatterns = [
     path('', views.select_subject, name='select-subject'),
-    path('<int:pk>/', Select_Subject.as_view(), name='groups-subject'),
-    path('<int:pk>/rules', Set_Rules.as_view(), name='groups-rules'),
+    path('<int:sub_id>/', views.Select_Subject, name='groups-subject'),
+    path('<int:sub_id>/rules', views.Set_Rules, name='groups-rules'),
     path('<int:pk>/stages', Set_Stages.as_view(), name='groups-stages'),
 ]
--- PTI/Semanas/semana_4/Floofy/floofy_project/groups/views.py
+++ PTI/Semanas/semana_4/Floofy/floofy_project/groups/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User as default_user
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -13,62 +13,41 @@ from django.views.generic import (
 def select_subject(request):
     return render(request, 'groups/sel-sub.html')
 
-class Set_Rules(UpdateView):
-    model = Subject
-    template_name = 'groups/groups-rules.html'
-    fields = ['groups_max', 'groups_deadline', 'groups_on']
-    
-    def post(self, request, **kwargs):
-        groups_max = request.POST.get('groups_max')
-        groups_deadline = request.POST.get('groups_deadline')
-        groups_on = request.POST.get('groups_on')
-        Group.objects.get()
-        return super(Set_Rules, self).post(request, **kwargs)
+def Select_Subject(request,sub_id=None):
+    if request.method == "GET":
+        try:
+            subject = Subject.objects.get(id=sub_id)
+        except:
+            raise Http404
+        context = {}
+        context['subject'] = subject
+        return render(request, 'groups/groups-sub.html', context)
 
-   
+def Set_Rules(request,sub_id=None):
+    if request.method == "GET":
+        try:
+            subject = Subject.objects.get(id=sub_id)
+        except:
+            raise Http404
+        context = {}
+        context['subject'] = subject
+        return render(request, 'groups/groups-rules.html', context)
+    if request.method == "POST":
+        sub = Subject.objects.get(id=sub_id)
+        if request.POST.get('groups_max'):
+            sub.groups_max = request.POST.get('groups_max')
+        if request.POST.get('groups_deadline'):
+            sub.groups_deadline = request.POST.get('groups_deadline')
+        if request.POST.get('groups_on'):
+            sub.groups_on = request.POST.get('groups_on')
+        sub.save()
+        context = {}
+        context['subject'] = sub
+        return render(request, 'groups/groups-rules.html', context)
 
-'''
-class Set_Rules(DetailView):
-    model = Subject
-    template_name = 'groups/groups-rules.html'
-    context_object_name = 'subject'
+        
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['groups'] = Group.objects.all()
-        context['users'] = User.objects.all()
-        context['subjects'] = Subject.objects.all()
-        context['form'] = DetailForm
-        return context
-    
 
-    def post(self, request, *args, **kwargs):
-        form = DetailForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Write Your Logic here
-            self.object = self.get_object()
-            context = super(Set_Rules, self).get_context_data(**kwargs)
-            context['form'] = DetailForm
-            return self.render_to_response(context=context)
-
-        else:
-            self.object = self.get_object()
-            context = super(Set_Rules, self).get_context_data(**kwargs)
-            context['form'] = form
-            return self.render_to_response( context=context)
-'''
-
-class Select_Subject(DetailView):
-    model = Subject
-    template_name = 'groups/groups-sub.html'
-    context_object_name = 'subject'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['groups'] = Group.objects.all()
-        context['users'] = User.objects.all()
-        context['subjects'] = Subject.objects.all()
-        return context
 
 class Set_Stages(DetailView):
     model = Subject
