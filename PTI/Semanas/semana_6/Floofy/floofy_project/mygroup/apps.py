c8205c6f40534955b513ea792bd49cb53a732e1b
fc51648 <fc51648@alunos.fc.ul.pt>
2020-04-21 15:57:34 +0100

    organizado repositorio

--- PTI/Semanas/semana_6/Floofy/floofy_project/mygroup/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MygroupConfig(AppConfig):
-    name = 'mygroup'
--- PTI/Semanas/semana_6/Floofy/floofy_project/mygroup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:sub_id>/', views.mygroup, name='mygroup'),
-    path('<int:sub_id>/leave-group/', views.leave_group, name='leave-group'),
-    path('<int:sub_id>/tasks/', views.tasks, name='tasks'),
-    path('<int:task_id>/this-task/', views.this_task, name='this-task'),
-]
